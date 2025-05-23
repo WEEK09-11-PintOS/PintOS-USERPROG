f4a0daa8dbc5272fdc6bd1f31a91f4e9f94c2f65
just-live28 <jkhkorea28@naver.com>
2025-05-23 17:21:55 +0900

    Fix: FDT_PAGES를 다시 3으로 변경

--- pintos-kaist/include/threads/thread.h
+++ pintos-kaist/include/threads/thread.h
@@ -30,7 +30,7 @@ typedef int tid_t;
 #define PRI_DEFAULT 31 /* Default priority. */
 #define PRI_MAX 63	   /* Highest priority. */
 
-#define FDT_PAGES 2
+#define FDT_PAGES 3
 #define FDT_COUNT_LIMIT 128
 #define MAX_FD FDT_PAGES * (1 << 9) // Limit fd_idx
 
